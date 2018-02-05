e9a7d23f25c5d371065dcd88c748d94ad74c8af6
Terrence Goh <terrence.goh@biqmind.com>
2018-02-05 10:52:52 +0800

    commit makefiles

---
+++ GNU ARM v4.9.3 - Default/sources.mk
@@ -0,0 +1,62 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+S79_SRCS := 
+OBJ_SRCS := 
+S_SRCS := 
+ASM_SRCS := 
+C_SRCS := 
+S_UPPER_SRCS := 
+O_SRCS := 
+EXECUTABLES := 
+OBJS := 
+C_DEPS := 
+
+# Every subdirectory with source files must be described here
+SUBDIRS := \
+adc/EFR32 \
+address-configuration-debug \
+antenna-stub \
+basic-server \
+button/EFR32 \
+button-press \
+cli \
+coap-cli \
+coap-dispatch \
+command-interpreter2 \
+connection-manager-cli \
+connection-manager-jib \
+debug-channel-stub/EFR32 \
+debug-print \
+dhcp-client \
+diagnostic-stub \
+eeprom \
+emdrv \
+emlib \
+external-generated-files \
+hal-library/EFR32 \
+heartbeat-node-type \
+icmp-cli \
+icmp-debug \
+identify-server \
+led/EFR32 \
+main \
+network-management-cli \
+on-off-server \
+ota-bootload-client \
+ota-bootload-client-policy \
+ota-bootload-core \
+ota-bootload-storage-cli \
+ota-bootload-storage-eeprom \
+. \
+serial/EFR32 \
+serial \
+sim-eeprom1/EFR32 \
+slaac-client \
+stack/thread-stack/EFR32 \
+stack/thread-stack \
+version-debug \
+zcl-core \
+zcl-core-cli \
+
